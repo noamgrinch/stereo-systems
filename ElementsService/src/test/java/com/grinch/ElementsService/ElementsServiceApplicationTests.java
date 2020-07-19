e3992e6888a552be3afb429f90ae2be79549036c
noamgrinch <noamgrinch@gmail.com>
2020-07-19 17:44:20 +0300

    Added skeleton of ElementsService.

---
+++ ElementsService/src/test/java/com/grinch/ElementsService/ElementsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grinch.ElementsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
